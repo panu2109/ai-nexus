c01d492ec1545c08c0b3568b38a191b2f4bf5009
panu2109 <prernadhanawade1@gmail.com>
2024-02-29 17:34:05 +0530

    commit

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -1,92 +1,181 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-
-import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
+import User from "../database/models/user.model";
+import Image from "../database/models/image.model";
+import { redirect } from "next/navigation";
 
-// CREATE
-export async function createUser(user: CreateUserParams) {
+import { v2 as cloudinary } from 'cloudinary'
+
+const populateUser = (query: any) => query.populate({
+  path: 'author',
+  model: User,
+  select: '_id firstName lastName clerkId'
+})
+
+// ADD IMAGE
+export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
 
-    const newUser = await User.create(user);
+    const author = await User.findById(userId);
 
-    return JSON.parse(JSON.stringify(newUser));
-  } catch (error) {
-    handleError(error);
-  }
-}
-
-// READ
-export async function getUserById(userId: string) {
-  try {
-    await connectToDatabase();
-
-    const user = await User.findOne({ clerkId: userId });
-
-    if (!user) throw new Error("User not found");
-
-    return JSON.parse(JSON.stringify(user));
-  } catch (error) {
-    handleError(error);
-  }
-}
-
-// UPDATE
-export async function updateUser(clerkId: string, user: UpdateUserParams) {
-  try {
-    await connectToDatabase();
-
-    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
-      new: true,
-    });
-
-    if (!updatedUser) throw new Error("User update failed");
-    
-    return JSON.parse(JSON.stringify(updatedUser));
-  } catch (error) {
-    handleError(error);
-  }
-}
-
-// DELETE
-export async function deleteUser(clerkId: string) {
-  try {
-    await connectToDatabase();
-
-    // Find user to delete
-    const userToDelete = await User.findOne({ clerkId });
-
-    if (!userToDelete) {
+    if (!author) {
       throw new Error("User not found");
     }
 
-    // Delete user
-    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
-    revalidatePath("/");
+    const newImage = await Image.create({
+      ...image,
+      author: author._id,
+    })
 
-    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+    revalidatePath(path);
+
+    return JSON.parse(JSON.stringify(newImage));
   } catch (error) {
-    handleError(error);
+    handleError(error)
   }
 }
 
-// USE CREDITS
-export async function updateCredits(userId: string, creditFee: number) {
+// UPDATE IMAGE
+export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase();
 
-    const updatedUserCredits = await User.findOneAndUpdate(
-      { _id: userId },
-      { $inc: { creditBalance: creditFee }},
+    const imageToUpdate = await Image.findById(image._id);
+
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+      throw new Error("Unauthorized or image not found");
+    }
+
+    const updatedImage = await Image.findByIdAndUpdate(
+      imageToUpdate._id,
+      image,
       { new: true }
     )
 
-    if(!updatedUserCredits) throw new Error("User credits update failed");
+    revalidatePath(path);
 
-    return JSON.parse(JSON.stringify(updatedUserCredits));
+    return JSON.parse(JSON.stringify(updatedImage));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// DELETE IMAGE
+export async function deleteImage(imageId: string) {
+  try {
+    await connectToDatabase();
+
+    await Image.findByIdAndDelete(imageId);
+  } catch (error) {
+    handleError(error)
+  } finally{
+    redirect('/')
+  }
+}
+
+// GET IMAGE
+export async function getImageById(imageId: string) {
+  try {
+    await connectToDatabase();
+
+    const image = await populateUser(Image.findById(imageId));
+
+    if(!image) throw new Error("Image not found");
+
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET IMAGES
+export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
+  limit?: number;
+  page: number;
+  searchQuery?: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    })
+
+    let expression = 'folder=imaginify';
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`
+    }
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {};
+
+    if(searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds
+        }
+      }
+    }
+
+    const skipAmount = (Number(page) -1) * limit;
+
+    const images = await populateUser(Image.find(query))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+    
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPage: Math.ceil(totalImages / limit),
+      savedImages,
+    }
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// GET IMAGES BY USER
+export async function getUserImages({
+  limit = 9,
+  page = 1,
+  userId,
+}: {
+  limit?: number;
+  page: number;
+  userId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find({ author: userId }))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find({ author: userId }).countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+    };
   } catch (error) {
     handleError(error);
   }
